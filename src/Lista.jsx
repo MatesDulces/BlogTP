1deedea64775b7c9784c7aa832f4891144458e50
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-03 14:23:34 -0300

    Update Lista.jsx

--- src/Lista.jsx
+++ src/Lista.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import { Link } from 'react-router-dom';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm'; 
 
