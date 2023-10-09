a9f05368abcb873f0336d81e555653695d6f312a
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-09 14:29:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Lista from './Lista';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import CommenBox from './CommenBox';
 //import AddPostForm from './AddPostForm'
 ReactDOM.createRoot(document.getElementById('root')).render(
