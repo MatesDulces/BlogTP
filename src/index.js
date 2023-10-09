f2b3a6de898f735c2461fc54ef4c845f0ccafc62
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-09 14:34:03 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<App />} />
-        <Route exact path='/Markdown' element={<Lista />} />
+        <Route exact path='/markdown' element={<Lista />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
