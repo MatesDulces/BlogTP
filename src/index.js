bf283395d18c2535168b03a6e94ff0c4fdf1e071
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-17 13:54:36 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import Lista from './Lista';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//import CommenBox from './CommenBox';
-//import AddPostForm from './AddPostForm'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route exact path='/' element={<App />} />
-        <Route exact path='/markdown' element={<Lista />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+
