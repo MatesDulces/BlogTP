47d43aedeb21fff71b94ec3bf5baba9194de361b
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-17 14:45:20 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,7 +8,7 @@ function Home(props) {
   const [almacen, setAlmacen] = useState([]);
 
   useEffect(() => {
-    let cosasGuardadas = JSON.parse(localStorage.getItem("post"));
+    let cosasGuardadas = JSON.parce(localStorage.getItem("post"));
     if (cosasGuardadas) setAlmacen(cosasGuardadas);
   }, []);
 
