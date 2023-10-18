eaeb30875f12e7f015d3be601318bf3c2fa94031
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-18 11:02:52 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,58 +1,73 @@
+import React, { useState, useEffect } from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-function Home(props) {
-  const { admin } = props;
-  const [almacen, setAlmacen] = useState([]);
+function Home({ admin, lista, lista2, setLista, setLista2 }) {
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    let cosasGuardadas = JSON.parse(localStorage.getItem("post"));
-    if (cosasGuardadas) setAlmacen(cosasGuardadas);
-  }, []);
+  const borrarItem = (index, lista, setLista) => {
+    const newList = [...lista];
+    newList.splice(index, 1);
+    setLista(newList);
+    localStorage.setItem("lista", JSON.stringify(newList));
+  };
 
-  function Home(cosa) {
-    console.log(cosa.id);
-    let delet = almacen.filter((i) => i.id !== cosa.id);
-    setAlmacen(delet);
-    localStorage.setItem("post", JSON.stringify(delet));
-    localStorage.setItem(`comentarios${cosa.id}`, JSON.stringify([]));
-  }
+  const agregarTexto = (texto, lista, setLista) => {
+    if (texto.trim() !== "") {
+      setLista([...lista, { text: texto, type: "item" }]);
+      localStorage.setItem("lista", JSON.stringify(lista));
+    }
+  };
 
   return (
     <>
       <nav>
         <ul className="lista">
           <li>
-            <Link className="Home" to="/">Volver a la pagina principal</Link>
+            <Link className="Home" to="/">Volver a la página principal</Link>
           </li>
           <li>
-            <Link className="Post" to="/Post">Ir a pagina de Markdown</Link>
+            <Link className="Post" to="/post">Ir a página de Markdown</Link>
           </li>
-          <li>
-            <Link className="Admin" to="/Admin">Ir a modo Admin</Link>
-          </li>
+          {admin && (
+            <li>
+              <Link className="Admin" to="/admin">Ir a modo Admin</Link>
+            </li>
+          )}
         </ul>
       </nav>
-      {almacen.map((cosa) => (
-        <>
-          <Link to={`/post/${cosa.id}`}>
-            <div>
-              <h1>{cosa.title}</h1>
-              <h2>{cosa.name}</h2>
-              <span> {cosa.id}</span>
-              <Markdown remarkPlugins={[remarkGfm]}>
-                {cosa.text.substring(0, 80) + "..."}
-              </Markdown>
-
-              <hr />
-            </div>
-          </Link>
-          {admin && <button onClick={() => HandleClick(cosa)}>BORRAR</button>}
-        </>
-      ))}
-      {admin && <h1>sos admin</h1>}
+      <h1>Página Principal</h1>
+      <h2>BLOG</h2>
+      <ul>
+        {lista.map((item, index) => (
+          <li key={index}>
+            {item.text}
+            {admin && (
+              <button onClick={() => borrarItem(index, lista, setLista)}>
+                BORRAR
+              </button>
+            )}
+          </li>
+        )}
+      </ul>
+      <h2>Comentarios</h2>
+      <ul>
+        {lista2.map((item, index) => (
+          <li key={index}>{item.text}</li>
+        )}
+      </ul>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input
+          placeholder="Agrega un nuevo elemento..."
+          type="text"
+          value={nombre}
+          onChange={(e) => setNombre(e.target.value)}
+        />
+        <button onClick={() => agregarTexto(nombre, lista, setLista)}>
+          AGREGAR
+        </button>
+      </form>
     </>
   );
 }
