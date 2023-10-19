75738cdfac5f6bd84a5d336f00f52c73caca13fb
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 11:36:54 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import './admin.css';
 
 function Admin(props) {
     const navigate = useNavigate();
