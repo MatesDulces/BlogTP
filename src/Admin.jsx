30f937f2f63e37f28d15fdf93448160c0c66da46
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 09:43:24 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Link } from 'react-router-dom';
-
-<h1>Pagina de Admin</h1>
-<p>Hola Thiago, hola</p>
+function Admin (){
+}
+return (
+  <h1>Pagina de Admin</h1>
+  <p>Hola Thiago, hola</p>
+  );
+export default Admin;
