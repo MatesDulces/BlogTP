b216db8ccb9fb44e5c0d1a834627f31ee7be2621
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 11:14:54 -0300

    Update Comentarios.jsx

--- src/Comentarios.jsx
+++ src/Comentarios.jsx
@@ -1,1 +1,16 @@
-
+        
+<form onSubmit={(e) => e.preventDefault()}>
+          <input
+            placeholder="Comentarios... ❤️"
+            type="text"
+            value={comentario}
+            onChange={(e) => setComentario(e.target.value)}
+          />
+          <button onClick={() => agregarTexto(comentario, lista2, setLista2)}>COMENTAR</button>
+        </form>
+        <h2>Comentarios</h2>
+        <ul>
+          {lista2.map((item, index) => (
+            <li key={index}>{item.text}</li>
+          ))}
+        </ul>
