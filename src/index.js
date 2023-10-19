b028dadb1e72de8a0a8e90df57d704aa169e28c0
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 10:59:49 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Rutas from "./Rutas";
 import { BrowserRouter} from "react-router-dom";
+import Home from "./Home";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
