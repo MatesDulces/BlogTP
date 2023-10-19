03773479400d4eb272d725fc706c2fea36c0ab3c
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 09:40:22 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,2 +1,7 @@
+import React, { useState, useEffect } from "react";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Link } from 'react-router-dom';
+
 <h1>Pagina de Admin</h1>
 <p>Hola Thiago, hola</p>
