b3e2efb5254618326f2cbaffbf6e96b40137a53d
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 11:50:21 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-function Admin() {
+function Admin(props) {
     const navigate = useNavigate();
     const { setAdmin } = props;
     const [pass, setPass] = useState("");
