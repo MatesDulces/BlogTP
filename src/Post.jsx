577eca308ee0efdfc03e5f41215abd3019a36e9f
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 11:08:12 -0300

    Update Post.jsx

--- src/Post.jsx
+++ src/Post.jsx
@@ -40,23 +40,6 @@ function Post() {
           ))}
         </ul>
       </div>
-
-      <div>
-        <form onSubmit={(e) => e.preventDefault()}>
-          <input
-            placeholder="Comentarios... ❤️"
-            type="text"
-            value={comentario}
-            onChange={(e) => setComentario(e.target.value)}
-          />
-          <button onClick={() => agregarTexto(comentario, lista2, setLista2)}>COMENTAR</button>
-        </form>
-        <h2>Comentarios</h2>
-        <ul>
-          {lista2.map((item, index) => (
-            <li key={index}>{item.text}</li>
-          ))}
-        </ul>
         <nav>
           <ul className="lista">
             <li>
@@ -67,7 +50,6 @@ function Post() {
             </li>
           </ul>
         </nav>
-      </div>
     </div>
   );
 }
