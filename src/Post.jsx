50155acf46b1411f04e003245f8a0793961b4768
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 11:02:39 -0300

    Update Post.jsx

--- src/Post.jsx
+++ src/Post.jsx
@@ -62,6 +62,9 @@ function Post() {
             <li>
               <Link className="Home" to="/">Volver a la página principal</Link>
             </li>
+            <li>
+              <Link className="Post" to="/post">Ir a página de Markdown</Link>
+            </li>
           </ul>
         </nav>
       </div>
@@ -69,4 +72,47 @@ function Post() {
   );
 }
 
-export default Post;  
+export default Post;   en este tro codigo import React, { useState, useEffect } from "react";
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
+    setPosts(postsGuardados);
+  }, []);
+
+  return (
+    <div className="home">
+      <header>
+        <img src="https://cdn4.iconfinder.com/data/icons/communication-v2/64/number_numero_count_thirty_five-2-512.png" alt="logo" />
+        <nav className="nav-menu">
+          <ul>
+            <li>
+              <Link to="/">Volver a la página principal</Link>
+            </li>
+            <li>
+              <Link to="/post">Publicar tu post</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      {posts.map((post, index) => (
+        {lista2.map((item, index) => (
+        <div className="container" key={index}>
+          <div className="post-item">
+            <Markdown remarkPlugins={[remarkGfm]}>
+              {post.text}
+            </Markdown>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Home;
