427f89bbde21970d941ad31540ef917ff3ff1005
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 22:45:14 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,57 +1,58 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-function Admin() {
-  const navigate = useNavigate();
-  const [admin, setAdmin] = useState(false);
-  const [contra, setContra] = useState('');
-  const [error, setError] = useState(false);
+function Admin(props) {
+    const navigate = useNavigate();
+    const { setAdmin } = props;
+    const [pass, setPass] = useState("");
+    const [error, setError] = useState(false);
+    const [ver, setVer] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (contra === 'VTM') { // Cambia la contraseña aquí
-      setAdmin(true);
-      navigate('/');
-    } else {
-      setAdmin(false);
-      setError(true);
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (pass === "VTM") {
+            setAdmin(true);
+            navigate('/');
+        } else {
+            setAdmin(false);
+            setError(true);
+        }
     }
-  }
 
-  function viewPass(e) {
-    e.preventDefault();
-    setVer(!ver);
-  }
+    function viewPass(e) {
+        e.preventDefault();
+        setVer(!ver);
+    }
 
-  return (
-    <>
-      <header>
-        <nav className="admin">
-          <ul>
-            <li>
-              <Link to="/">Volver a la página principal</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      {error && <h1 className="error">CONTRASEÑA INCORRECTA</h1>}
-      <form className="form">
-        <input
-          className="input"
-          value={contra}
-          type={ver ? 'text' : 'password'}
-          onChange={(e) => setContra(e.target.value)}
-        />
-        <button className="button" onClick={handleSubmit}>
-          Enviar
-        </button>
-        <button className="button" onClick={viewPass}>
-          {ver ? 'Ocultar' : 'Ver'}
-        </button>
-      </form>
-    </>
-  );
+    return (
+        <>
+            <header>
+                <nav className="nav-menu">
+                    <ul>
+                        <li>
+                            <Link to="/">Volver a la página principal</Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="div">
+                {error && (<h1 className="error">CONTRASEÑA INCORRECTA</h1>)}
+                <form className="form">
+                    <input
+                        className="input"
+                        value={pass}
+                        type={ver ? "text" : "password"} 
+                        onChange={e => setPass(e.target.value)}
+                    />
+                    <button className="button" onClick={handleSubmit}>Enviar</button>
+                    <button className="button" onClick={viewPass}>
+                        {ver ? "Ocultar" : "Ver"}
+                    </button>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default Admin;
