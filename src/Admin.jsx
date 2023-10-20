fefce41a38b7d4a30c572fae7c36c9585ffc0649
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 20:28:32 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAdminContext } from "./AdminContext";
+import './admin.css';
 
 function Admin(props) {
   const { setAdmin } = props;
