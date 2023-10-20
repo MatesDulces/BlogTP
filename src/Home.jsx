087a34029229708ee1644f6ab79a08071d49c70d
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 12:28:01 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,29 +3,25 @@ import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Link, useParams } from 'react-router-dom';
 import './Home.css';
+import Admin from "./Admin";
 
 function Home() {
   const [posts, setPosts] = useState([]);
+  const [admin, setAdmin] = useState(false);
   const [lista, setLista] = useState([]);
-  const [lista2, setLista2] = useState([]);
-  const isAdmin = localStorage.getItem("admin") === "true"; // Verifica si estás en modo administrador
-
-  function HandleClick(post) {
-    let elim = lista.filter((i) => i.id !== post.id);
-    setLista(elim);
-    localStorage.setItem("lista", JSON.stringify(elim));
-    localStorage.removeItem(`comentarios${post.id}`);
-  }
-
+  const [lista2, setLista2] = useState ([]);
+  const [titulo, setTitulo] = useState ([]);
+  
   useEffect(() => {
     let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
     setPosts(postsGuardados.reverse());
   }, []);
 
-  useEffect(() => {
-    let tituloGuardado = JSON.parse(localStorage.getItem("lista2")) || [];
-    setLista2(tituloGuardado.reverse());
+ useEffect(() => {
+    let titulosGuardados = JSON.parse(localStorage.getItem("lista2")) || [];
+    setTitulo(titulosGuardados.reverse());
   }, []);
+
 
   const deletePost = (postId) => {
     const updatedPosts = posts.filter(post => post.id !== postId);
@@ -35,7 +31,7 @@ function Home() {
 
   return (
     <div className="home">
-      {isAdmin && <h1 className='admin-mode'>Modo Administrador</h1>} {/* Muestra el cartel si estás en modo administrador */}
+      {admin && <h1 className='administrador'>Modo Administrador</h1>}
       <h1 className="Twitter">Twitter 2</h1>
       <header>
         <nav className="nav-menu">
@@ -46,11 +42,11 @@ function Home() {
           </ul>
         </nav>
       </header>
-      {lista.map((lista2, index1) => (
+      {lista.map((titulo, index1) => (
         <div className="containe" key={index1}>
-          <div className="lista2-item">
+          <div className="titulo-item">
             <Markdown remarkPlugins={[remarkGfm]}>
-              {lista2.text}
+              {titulo}
             </Markdown>
             <button className="BORRAR" onClick={() => HandleClick(item)}>Borrar</button>
           </div>
@@ -63,6 +59,7 @@ function Home() {
               {post.text}
             </Markdown>
             <div>
+              {admin && ( <button className="BORRAR" onClick={() => deletePost(post.id)}>Borrar</button> )}
               <Link to={`/comentar/${post.id}`}>Comentar</Link>
             </div>
           </div>
