646a3733de0fa25da96284146c18c1d9aeaa3fc3
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 23:36:58 -0300

    Update Comentarios.jsx

--- src/Comentarios.jsx
+++ src/Comentarios.jsx
@@ -1,26 +1,34 @@
-/*import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Link } from 'react-router-dom';
-function Comentarios (){
-        return (
-<form onSubmit={(e) => e.preventDefault()}>
-        <>
-                
-          <input
-            placeholder="Comentarios..."
-            type="text"
-            value={comentario}
-            onChange={(e) => setComentario(e.target.value)}
-          />
-          <button onClick={() => agregarTexto(comentario, lista2, setLista2)}>COMENTAR</button>
-        <h2>Comentarios</h2>
-        <ul>
-          {lista2.map((item, index) => (
-            <li key={index}>{item.text}</li>
-          ))}
-        </ul>
-        </>
-                );
-                }
-*/
+
+function Comentarios(index) {
+    let elim = posts.filter((i) => i.id !== index.id);
+    setPosts(elim);
+    setComments({ ...comments, [index.id]: [] }); // Borra los comentarios asociados al post
+    localStorage.setItem("post", JSON.stringify(elim));
+    localStorage.setItem(`comentarios${index.id}`, JSON.stringify([]));
+  }
+
+  function handleAddComment(index, commentText) {
+    const newComment = { text: commentText, id: Date.now() };
+    const updatedComments = [...comments[index.id], newComment];
+    setComments({ ...comments, [index.id]: updatedComments });
+    localStorage.setItem(`comentarios${index.id}`, JSON.stringify(updatedComments));
+  }
+
+  useEffect(() => {
+    let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
+    setPosts(postsGuardados.reverse());
+
+    const commentsData = {};
+    postsGuardados.forEach((post) => {
+      const postComments = JSON.parse(localStorage.getItem(`comentarios${post.id}`)) || [];
+      commentsData[post.id] = postComments;
+    });
+    setComments(commentsData);
+  }, []);
+
+export default Comentarios;
+
