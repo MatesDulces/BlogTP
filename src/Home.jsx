8fa604cfb80cfee6b41f65d16cff3161a9db1239
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 22:27:20 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,23 +10,22 @@ function Home() {
   const [admin, setAdmin] = useState(false); 
   
   function HandleClick(index) {
-    console.log(index.id);
-    let elim = lista.filter((i) => i.id !== index.id);
-    setLista(elim);
-    localStorage.setItem("post", JSON.stringify(elim));
-    localStorage.setItem(`comentarios${index.id}`, JSON.stringify([]));
+    const postToDelete = posts[index];
+    const updatedPosts = posts.filter((_, i) => i !== index);
+    setPosts(updatedPosts);
+    localStorage.setItem("post", JSON.stringify(updatedPosts));
+    localStorage.removeItem(`comentarios${postToDelete.id}`);
   }
 
-
   useEffect(() => {
-    let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
+    let postsGuardados = JSON.parse(localStorage.getItem("post")) || [];
     setPosts(postsGuardados.reverse());
   }, []);
 
   return (
     <div className="home">
-      {admin && <h1 classname = 'administrador'>Modo Administrador</h1>}
-      <h1 className = "Twitter">Twitter 2</h1>
+      {admin && <h1 className='administrador'>Modo Administrador</h1>}
+      <h1 className="Twitter">Twitter 2</h1>
       <header>
         <nav className="nav-menu">
           <ul>
@@ -40,7 +39,7 @@ function Home() {
         </nav>
       </header>
       {posts.map((post, index) => (
-        <div className="container" key={index}>
+        <div className="container" key={post.id}>
           <div className="post-item">
             <Markdown remarkPlugins={[remarkGfm]}>
               {post.text}
