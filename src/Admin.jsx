5b08b8d1eabb5d26ed95eeddf999d4d7e9475799
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 22:05:59 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 function Admin(props) {
   const navigate = useNavigate();
   const { setAdmin } = props;
   const [pass, setPass] = useState("");
   const [error, setError] = useState(false);
-  const [ver, setVer] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -15,42 +13,30 @@ function Admin(props) {
       setAdmin(true);
       navigate('/');
     } else {
-      setAdmin(false);
       setError(true);
     }
   }
 
-  function viewPass(e) {
-    e.preventDefault();
-    setVer(!ver);
-  }
-
   return (
     <>
-      <header>
-        <nav className="nav-menu">
-          <ul>
-            <li>
-              <Link to="/">Volver a la página principal</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className="div">
-        {error && (<h1 className="error">CONTRASEÑA INCORRECTA</h1>)}
-        <form className="form">
-          <input
-            className="input"
-            value={pass}
-            type={ver ? "text" : "password"} 
-            onChange={e => setPass(e.target.value)}
-          />
-          <button className="button" onClick={handleSubmit}>Enviar</button>
-          <button className="button" onClick={viewPass}>
-            {ver ? "Ocultar" : "Ver"}
-          </button>
-        </form>
-      </div>
+      {props.admin ? (
+        <div>
+          {/* Contenido de administrador */}
+        </div>
+      ) : (
+        <div>
+          {error && (<h1 className="error">CONTRASEÑA INCORRECTA</h1>)}
+          <form className="form" onSubmit={handleSubmit}>
+            <input
+              className="input"
+              value={pass}
+              type="password"
+              onChange={e => setPass(e.target.value)}
+            />
+            <button type="submit" className="button">Ingresar</button>
+          </form>
+        </div>
+      )}
     </>
   );
 }
