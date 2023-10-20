bd0d9ed6ba7ff6fecc17fb347c0f6c1efa533812
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 12:11:06 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-/*import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Link, useParams } from 'react-router-dom';
@@ -10,6 +10,7 @@ function Home() {
   const [admin, setAdmin] = useState(false);
   const [lista, setLista] = useState([]);
   const [lista2, setLista2] = useState ([]);
+  const [titulo, setTitulo] = useState ([]);
 
   function HandleClick(post) {
     let elim = lista.filter((i) => i.id !== post.id);
@@ -24,8 +25,8 @@ function Home() {
   }, []);
 
  useEffect(() => {
-    const storedLista2 = JSON.parse(localStorage.getItem("lista2")) || [];
-    setLista2(storedLista2);
+    let titulosGuardados = JSON.parse(localStorage.getItem("lista2")) || [];
+    setTitulo(titulosGuardados.reverse());
   }, []);
 
 
@@ -48,11 +49,11 @@ function Home() {
           </ul>
         </nav>
       </header>
-      {lista.map((lista2, index1) => (
+      {lista.map((titulo, index1) => (
         <div className="containe" key={index1}>
-          <div className="lista2-item">
+          <div className="titulo-item">
             <Markdown remarkPlugins={[remarkGfm]}>
-              {lista2.text}
+              {titulo.text}
             </Markdown>
             <button className="BORRAR" onClick={() => HandleClick(item)}>Borrar</button>
           </div>
@@ -75,87 +76,5 @@ function Home() {
   );
 }
 
-export default Home;*/
-
-import React, { useState, useEffect } from "react";
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Link, useParams } from 'react-router-dom';
-import './Home.css';
-import Admin from "./Admin";
-
-function Home() {
-  const [posts, setPosts] = useState([]);
-  const [lista, setLista] = useState([]);
-  const [lista2, setLista2] = useState([]);
-
-  // Aquí establece la variable de estado 'admin'
-  const [admin, setAdmin] = useState(false);
-
-  function HandleClick(post) {
-    let elim = lista.filter((i) => i.id !== post.id);
-    setLista(elim);
-    localStorage.setItem("lista", JSON.stringify(elim));
-    localStorage.removeItem(`comentarios${post.id}`);
-  }
-
-  useEffect(() => {
-    let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
-    setPosts(postsGuardados.reverse());
-  }, []);
-
-  useEffect(() => {
-    let tituloGuardado = JSON.parse(localStorage.getItem("lista2")) || [];
-    setLista2(tituloGuardado.reverse());
-  }, []);
-
-  const deletePost = (postId) => {
-    const updatedPosts = posts.filter(post => post.id !== postId);
-    setPosts(updatedPosts);
-    localStorage.setItem("lista", JSON.stringify(updatedPosts));
-  };
-
-  return (
-    <div className="home">
-      {admin && <h1 className='administrador'>Modo Administrador</h1>}
-      <h1 className="Twitter">Twitter 2</h1>
-      <header>
-        <nav className="nav-menu">
-          <ul>
-            <li>
-              <Link to="/post">Publicar tu post</Link>
-            </li>
-            {/* Agrega un botón para ir a la página de administrador */}
-            <li>
-              <Link to="/admin">Administrador</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      {lista.map((lista2, index1) => (
-        <div className="container" key={index1}>
-          <div className="lista2-item">
-            <Markdown remarkPlugins={[remarkGfm]}>
-              {lista2.text}
-            </Markdown>
-            <button className="BORRAR" onClick={() => HandleClick(item)}>Borrar</button>
-          </div>
-        </div>
-      ))}
-      {posts.map((post, index) => (
-        <div className="container" key={post.id}>
-          <div className="post-item">
-            <Markdown remarkPlugins={[remarkGfm]}>
-              {post.text}
-            </Markdown>
-            <div>
-              <Link to={`/comentar/${post.id}`}>Comentar</Link>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
-
 export default Home;
+
