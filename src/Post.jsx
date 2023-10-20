709485c459446fe83d079dce1b21839000388938
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 10:59:19 -0300

    Update Post.jsx

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,84 +1,3 @@
-/*import React, { useState, useEffect } from "react";
-import Markdown from 'react-markdown';
-import remarkGfm from 'remark-gfm';
-import { Link } from 'react-router-dom';
-import './Post.css';
-
-function Post() {
-  const [titulo, setTitulo] = useState("");  
-  const [nombre, setNombre] = useState("");
-  const [lista, setLista] = useState([]);
-  const [lista2, setLista2] = useState([]);  
-  const [markdownPreview, setMarkdownPreview] = useState("");
-
-  useEffect(() => {
-    const storedLista = JSON.parse(localStorage.getItem("lista")) || [];
-    setLista(storedLista);
-  }, []);
-
-  const agregarTexto = (titulo, texto) => { 
-    if (titulo.trim() !== "" && texto.trim() !== "") {
-      const nuevoItem = { title: titulo, text: texto, type: "item" };
-      const updatedLista = [...lista, nuevoItem];
-      setLista(updatedLista);
-      localStorage.setItem("lista", JSON.stringify(updatedLista));
-
-
-      const updatedLista2 = [...lista2, titulo];
-      setLista2(updatedLista2);
-      setTitulo("");  
-      setNombre("");
-      setMarkdownPreview("");
-    }
-  };
-
-  return (
-    <div className="container">
-      <div> 
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            agregarTexto(titulo, nombre);  
-          }}
-        >
-          <input
-            type="text"
-            placeholder="TÍTULO"
-            value={titulo}
-            onChange={(e) => setTitulo(e.target.value)}
-          />
-          <textarea
-            placeholder="ESCRIBE EN MARKDOWN 😊"
-            rows="5"
-            value={nombre}
-            onChange={(e) => {
-              setNombre(e.target.value);
-              setMarkdownPreview(e.target.value);
-            }}
-          />
-          <Markdown remarkPlugins={[remarkGfm]}>{markdownPreview}</Markdown>
-          <button type="submit">AGREGAR</button>
-        </form>
-      </div>
-      <nav>
-        <ul className="lista">
-          <li>
-            <Link className="Home" to="/">
-              Volver a la página principal
-            </Link>
-          </li>
-          <li>
-            <Link className="Post" to="/post">
-              Ir a página de Markdown
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-}
-
-export default Post;*/
 
 import React, { useState, useEffect } from "react";
 import Markdown from 'react-markdown';
@@ -98,7 +17,6 @@ function Post() {
     const storedLista = JSON.parse(localStorage.getItem("lista")) || [];
     setLista(storedLista);
 
-    // Calcula el último ID existente y establece el estado
     const maxId = storedLista.reduce((max, post) => (post.id > max ? post.id : max), 0);
     setLastId(maxId);
   }, []);
@@ -106,7 +24,7 @@ function Post() {
   const agregarTexto = (titulo, texto) => { 
     if (titulo.trim() !== "" && texto.trim() !== "") {
       const nuevoItem = { 
-        id: lastId + 1, // Asigna un nuevo ID único
+        id: lastId + 1, 
         title: titulo, 
         text: texto, 
         type: "item" 
@@ -114,7 +32,7 @@ function Post() {
 
       const updatedLista = [...lista, nuevoItem];
       setLista(updatedLista);
-      setLastId(lastId + 1); // Incrementa el último ID
+      setLastId(lastId + 1); 
       localStorage.setItem("lista", JSON.stringify(updatedLista));
       
       const updatedLista2 = [...lista2, titulo];
