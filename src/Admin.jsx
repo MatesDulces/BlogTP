c21f99aeb619aa3e7782d7aa807cbca3a287deee
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 12:13:58 -0300

    Update Admin.jsx

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,57 +1,47 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Admin(props) {
+function Admin() {
   const navigate = useNavigate();
-  const { setAdmin } = props;
-  const [pass, setPass] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
-  const [ver, setVer] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    if (pass === "VTM") {
-      setAdmin(true);
-      navigate('/');
+  const handleLogin = () => {
+    if (password === "VTM") {
+      // Establece el estado de administrador a true
+      localStorage.setItem("admin", "true");
+      navigate("/");
     } else {
-      setAdmin(false);
       setError(true);
     }
-  }
+  };
 
-  function viewPass(e) {
-    e.preventDefault();
-    setVer(!ver);
-  }
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
-    <>
-      <header>
-        <nav className="nav-menu">
-          <ul>
-            <li>
-              <Link to="/">Volver a la página principal</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <div className="div">
-        {error && <h1 className="error">CONTRASEÑA INCORRECTA</h1>}
-        <form className="form">
+    <div className="admin-page">
+      <div className="admin-container">
+        <h1>Admin Login</h1>
+        {error && <p className="error-message">Contraseña incorrecta</p>}
+        <div>
+          <label htmlFor="password">Contraseña</label>
           <input
-            className="input"
-            value={pass}
-            type={ver ? "text" : "password"}
-            onChange={e => setPass(e.target.value)}
+            type={showPassword ? "text" : "password"}
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="button" onClick={handleSubmit}>Enviar</button>
-          <button className="button" onClick={viewPass}>
-            {ver ? "Ocultar" : "Ver"}
-          </button>
-        </form>
+        </div>
+        <button onClick={handleLogin}>Ingresar</button>
+        <button onClick={togglePasswordVisibility}>
+          {showPassword ? "Ocultar Contraseña" : "Mostrar Contraseña"}
+        </button>
+        <Link to="/">Volver a la página principal</Link>
       </div>
-    </>
+    </div>
   );
 }
 
