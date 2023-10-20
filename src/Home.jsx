861f7994e070fbb9b6156f4047b71a3c6b8ae7ee
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-19 22:00:20 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,18 +3,16 @@ import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Link } from 'react-router-dom';
 import './Home.css';
-import Admin from "./Admin";
 
 function Home() {
   const [posts, setPosts] = useState([]);
-  const [admin, setAdmin] = useState(false); 
-  
- function  HandleClick(index) {
-    const updatedList = [...this.state.listaElementos];
-    updatedList.splice(index, 1);
-    this.setState({ listaElementos: updatedList });
-  }
+  const [admin, setAdmin] = useState(false);
 
+  function HandleClick(index) {
+    const updatedList = [...posts];
+    updatedList.splice(index, 1);
+    setPosts(updatedList);
+  }
 
   useEffect(() => {
     let postsGuardados = JSON.parse(localStorage.getItem("lista")) || [];
@@ -23,8 +21,8 @@ function Home() {
 
   return (
     <div className="home">
-      {admin && <h1 classname = 'administrador'>Modo Administrador</h1>}
-      <h1 className = "Twitter">Twitter 2</h1>
+      {admin && <h1 className="administrador">Modo Administrador</h1>}
+      <h1 className="Twitter">Twitter 2</h1>
       <header>
         <nav className="nav-menu">
           <ul>
