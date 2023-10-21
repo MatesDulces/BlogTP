c5eac1ab7037a429ca256b80e355b968fa0d2461
papu <145486019+MatesDulces@users.noreply.github.com>
2023-10-20 21:49:46 -0300

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -45,7 +45,7 @@ function Home(props) {
       {lista.map((item, index1) => (
         <div className="container" key={index1}>
           <div className="lista2-item">
-            <h2>{item.title}</h2>
+            <h2>Autor: {item.title}</h2>
             <Markdown remarkPlugins={[remarkGfm]}>
               {item.text}
             </Markdown>
